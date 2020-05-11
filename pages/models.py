3b01f38201754ae1370684cafb58f4005e5c8b35
Rashid Hamid Dar <rashidhamid139@gmail.com>
2020-05-11 15:12:09 +0000

    Add files via upload
    Updated Model File

--- pages/models.py
+++ pages/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+
+    def __str__(self):
+        return self.username
